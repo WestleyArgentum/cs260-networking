3ef6aaa1fb25b66f35a7ca9a0c8efa3fbc444ddb
knuxjr <knuxjr@1f2afcba-5144-80f4-e828-afee2f9acc6f>
2010-03-26 05:15:59 +0000

    Haxor

---
+++ trunk/client/westley.hennigh_cs260/westley.hennigh_cs260/udp.cpp
@@ -0,0 +1,54 @@
+#include "udp.h"
+#include "stdio.h"
+#include <cstring>
+
+void UDP::write(void)
+{
+  char pub[] = "p:";
+  strcat(pub, file);
+
+  FILE* in = fopen(file, "rb");
+  FILE* out = fopen(pub, "wb" );
+  fseek (in, 0, SEEK_END);
+
+  char* buffer = new char[ftell(in)];
+
+  if(in == NULL || out == NULL)
+  {
+      perror( "An error occured while opening files!!!" );
+      return;
+  }
+
+  size_t len = 0;
+  while((len = fread(buffer, BUFSIZ, sizeof(char), in)) > 0 )
+      fwrite(buffer, BUFSIZ, sizeof(char), out);
+
+  delete [] buffer;
+  fclose(in);
+  fclose(out);
+}
+void UDP::read(void)
+{
+  char pub[] = "p:";
+  strcat(pub, file);
+
+  FILE* in = fopen(pub, "rb");
+  FILE* out = fopen(file, "wb" );
+  fseek(in, 0, SEEK_END);
+
+  char* buffer = new char[ftell(in)];
+
+  if(in == NULL || out == NULL)
+  {
+      perror( "An error occured while opening files!!!" );
+      return;
+  }
+
+  size_t len = 0;
+  while((len = fread(buffer, BUFSIZ, sizeof(char), in)) > 0 )
+      fwrite(buffer, BUFSIZ, sizeof(char), out);
+
+  delete [] buffer;
+  fclose(in);
+  fclose(out);
+}
